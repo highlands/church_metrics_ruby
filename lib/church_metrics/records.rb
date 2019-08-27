0771ea321ef4948bdfa7311456dd2f337a48f111
Joshua Perdue <joshua.perdue@gmail.com>
2019-08-27 14:47:13 -0500

    [DM-2] Updates to CM api calls

--- lib/church_metrics/records.rb
+++ lib/church_metrics/records.rb
@@ -3,7 +3,5 @@ module ChurchMetrics
     # optional params for Records
     # start_time, end_time, start_week, end_week, category_id, event_id, campus_id, week_reference
 
-    # https://github.com/lifechurch/churchmetrics-api/blob/master/sections/records.md#get-records
-
   end
 end
